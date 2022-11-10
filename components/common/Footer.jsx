59f39790ee072219b902ebc6f26d5cb15f4b8240
Perfect Man <57462177+bestmycode@users.noreply.github.com>
2022-11-10 13:33:15 -0300

    update task 4~8

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
 
           <div className="footer-right">
             <ul className="flex items-center flex-col sm:flex-row sm:space-x-10 lg:justify-start justify-center">
-              <li className="mb-4 sm:mb-0">Privacy Policy</li>
+              <li className="mb-4 sm:mb-0"><a href="https://vice-web-statics-cdn.vice.com/privacy-policy/en_us/page/privacy-policy.html">Privacy Policy</a></li>
               <li>About vice</li>
             </ul>
 
